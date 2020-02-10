bdb7a12ca46c033f0a784a3f4f344b566d80202c
msimon42 <msimon@uab.edu>
2020-02-10 10:15:14 -0700

    Add feature tests

--- spec/features/house_search_spec.rb
+++ spec/features/house_search_spec.rb
@@ -1,7 +1,20 @@
 RSpec.describe 'when i search for all members of a house' do
   describe 'i get a list of all members of that house and various attributes' do
+    before :each do
+      visit '/search?house=Gryffindor'
     it 'has a list of 21 members when i search for gryffindor' do
-      
+      within("#members") do
+        expect(page).to have_css('li', count: 21)
+      end
+    end
+
+    it 'displays info on member' do
+      within("#member-5a1096b33dc2080021cd8762") do
+        expect(page).to have_content("Aberforth Dumbledore")
+        expect(page).to have_content("Owner, Hog's Head Inn")
+        expect(page).to have_content("goat")
+        expect(page).to have_content("Gryffindor")
+      end
     end
   end
 end
