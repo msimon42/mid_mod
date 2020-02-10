6d5863830c875ff19975ce75fcd1ec173d045428
msimon42 <msimon@uab.edu>
2020-02-10 09:38:09 -0700

    Add tests

---
+++ spec/features/house_search_spec.rb
@@ -0,0 +1,7 @@
+RSpec.describe 'when i search for all members of a house' do
+  describe 'i get a list of all members of that house and various attributes' do
+    it 'has a list of 21 members when i search for gryffindor' do
+      
+    end
+  end
+end
---
+++ spec/services/potter_service_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe 'potter_service' do
+  describe 'search for members by house' do
+    before :each do
+      @members = PotterService.new.search_by_house('Gryffindor')
+    end
+
+    it 'returns 21 members for gryffindor' do
+      expect(@members.length).to eq(21)
+    end
+
+    it 'returns info on each member' do
+      @members.each do |member|
+        expect(member.name).to be_truthy
+        expect(member.house).to eq('Gryffindor')
+    end
+  end
+end
